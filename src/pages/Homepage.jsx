ae80e9e4c3dc0f8e7be968dc974c399734b6392e
Simran Sharma <ssa13386@gmail.com>
2026-05-14 15:27:54 +0530

    homepage logo added

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -138,6 +138,7 @@ export default function Homepage() {
           <h1 className="text-3xl font-black text-orange-500">
             FlexiOrder
           </h1>
+          </div>
 <div className="relative flex justify-between items-center mb-16">
   
   {/* LOGO */}
